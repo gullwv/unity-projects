f418230fcecc7dcb555f50650abd2ac50d89ce26
gullwv <45007150+gullwv@users.noreply.github.com>
2020-01-28 10:50:57 -0500

    upload the create-with-code folder.

---
+++ Create-with-Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 20.0f; // speed of vehicle
+    private float turnSpeed = 90.0f; // turn speed of vehicle
+    private float horizontalInput; // input for left-right
+    private float verticalInput; // input for forward-back
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // get horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // forward motion for the vehicle.
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput); // go forward or backward at the speed in the "speed" float when up-down or w-s pressed
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput); //move left or right at the speed in the "turnSpeed" float when left-right or a-d pressed
+
+    }
+}
