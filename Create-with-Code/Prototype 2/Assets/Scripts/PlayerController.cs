9c32d07b587b6c1e693a955d592a9fb7a1ee45c4
gullwv <45007150+gullwv@users.noreply.github.com>
2020-02-11 12:30:07 -0500

    prototype 2 added.

---
+++ Create-with-Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float horizontalInput; //for checking horizontal input.
+    
+    public float speed = 10; //player movement speed
+    public float xRange = 10; //range of the boundaries
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get player input and move them
+        horizontalInput = Input.GetAxis("Horizontal"); //get input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); //move them
+        
+        // keep the player in bounds
+        if (transform.position.x < -xRange) // if the player tries to go left out of range, put em back
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } else if (transform.position.x > xRange) // if the player tries to go right out of range, put em back
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+        }
+
+    }
+}
---
+++ Create-with-Code/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // spawn an animal when S is pressed
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+            SpawnRandomAnimal();
+
+        }
+
+    }
+
+    void SpawnRandomAnimal()
+    {
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length); // random animal
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, 20);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation); // spawn animal.
+
+    }
+
+}
